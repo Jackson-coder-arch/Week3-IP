afcb758b531c2a2c5bf056c2727562f364d59a3c
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-04-04 21:35:27 +0300

    importation on view functions

--- award/views.py
+++ award/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import(
+    Project,
+    Profile,
+)
 
 def home(request):
-    return render(request, 'home.html')
+    projects = project.objects.all()
+    context = {"project_list": projects}
+    return render(request, 'home.html', context = context)
 
 def project(request):
-    return render(request, 'projects.html')
+    project = get_object_or_404(Project,description= description, )
+    return render(request, 'projects.html',{'project':project})
