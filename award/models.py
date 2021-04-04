c63c724b123c55f610004599f712c2d0646d9d0f
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-04-04 21:08:47 +0300

    addition of functions

--- award/models.py
+++ award/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
+class Project(models.Model):
     name = models.CharField(max_length=50)
     image = CloudinaryField('image')
     description =models.TextField(max_length=500)
     link =
 
 class Profile(models.Model):
-    profile_photo =
-    bio =
-    posts =
-    contact =
+    profile_photo = CloudinaryField('image')
+    bio =models.TextField(max_length= 300)
+    posts = models.CharField(max_length=100)
+    contact = models.EmailField
+
+class Rating(models.Model):
+    Post
+
+class Review(models.Model):
+
+
